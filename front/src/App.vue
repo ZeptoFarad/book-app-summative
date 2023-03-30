94f37fb3adfe0255d67a4b17e29cb26bd1afbe7e
Ke Guo <111708988+Gabriela2022Guo@users.noreply.github.com>
2023-03-30 22:36:23 +1300

    ke1

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,29 @@
+<script setup>
+ import HeaderVue from './assets/components/header.vue'
+ import bookcards from './assets/components/bookcards.vue'
+ import footerVue from './assets/components/footer.vue'
+</script>
+
+
 <template>
-  <div>
-    
+  <div class="home-box">
+     <div class="header-area">
+     <HeaderVue />
+     </div>
+     <div class="body-arae">
+     <bookcards />
+     </div>
+     <div class="footer-area">
+     <footerVue />
+     </div>
+  
   </div>
 </template>
+<style scoped>
+  .home-box{
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+  }
+
+</style>

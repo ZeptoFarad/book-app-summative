0df405e3a2414f76a44aa3d3dbbafd1f9ee9b2ea
Jeremys_Github_Account <104894365+ZeptoFarad@users.noreply.github.com>
2023-03-31 09:55:10 +1300

    Revert "Ke20022023 t"

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,29 +1,5 @@
-<script setup>
- import HeaderVue from './assets/components/header.vue'
- import bookcards from './assets/components/bookcards.vue'
- import footerVue from './assets/components/footer.vue'
-</script>
-
-
 <template>
-  <div class="home-box">
-     <div class="header-area">
-     <HeaderVue />
-     </div>
-     <div class="body-arae">
-     <bookcards />
-     </div>
-     <div class="footer-area">
-     <footerVue />
-     </div>
-  
+  <div>
+    
   </div>
 </template>
-<style scoped>
-  .home-box{
-    display: grid;
-    grid-template-rows: auto 1fr auto;
-    min-height: 100vh;
-  }
-
-</style>
--- front/src/assets/components/bookcards.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <h1>Book cards display area</h1>
-    
-    
-  
-  </template>

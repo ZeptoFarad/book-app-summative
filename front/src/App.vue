6fd30016680546c368a18742fd1c1e0979913f44
ZeptoFarad <104894365+ZeptoFarad@users.noreply.github.com>
2023-04-03 10:42:33 +1200

    Add Fonts Locally Instead Of CDN

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    liam test
+    <h1>Test</h1>
+    <h2>Test H2</h2>
+    <h3>Test H3</h3>
+    <h4>Test H4</h4>
+    <h5>Test H5</h5>
+    <p>Test p</p>
   </div>
 </template>

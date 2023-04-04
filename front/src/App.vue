6e8528a3d13ce8fac6dd252b18e3582233cfb2e5
LiamPatman <270025210@yoobeestudent.ac.nz>
2023-04-05 09:48:52 +1200

    PostConfirmedModal_Finished
    PostConfirmedModal_Finished

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,7 @@
-<script setup>
-import headerVue from './components/Header.vue'
-</script>
-
 <template>
-   <div class="header-area">
-     <headerVue />
+   <div>
+     
    </div>
-  <div>
-    push test
-  </div>
 </template>
 
 

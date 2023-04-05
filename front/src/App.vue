e8ab51eadb0b384525d2ef3cac1648587f9d334c
LiamPatman <270025210@yoobeestudent.ac.nz>
2023-04-06 11:39:09 +1200

    Footer - Completed

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
    import Footer from './components/Footer.vue';
-   import FooterAlt from './components/FooterAlt.vue';
-
 </script>
 
 <template>
-   <div>
-     <FooterAlt />
-   </div>
+   <footer>
+     <Footer />
+   </footer>
 </template>
 
 
--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,22 +1,28 @@
 <template>
     <div class="footer">
         <div class="account">
-            <h3>ACCOUNT</h3>
-            <h4>My Account</h4>
-            <h4>My Bookshop</h4>
-            <h4>Logout</h4>
+            <h3>ACCOUNT <i class="arrow"></i></h3>
+            <ul class="account-links">
+                <li><h4>My Account</h4></li>
+                <li><h4>My Bookshop</h4></li>
+                <li><h4>Logout</h4></li>
+            </ul>
         </div>
         <div class="about_us">
-            <h3>ABOUT US</h3>
-            <h4>About Peekabook</h4>
-            <h4>Careers</h4>
-            <h4>Privacy Policy</h4>
+            <h3>ABOUT US <i class="arrow"></i></h3>
+            <ul class="about-links">
+                <li><h4>About Peekabook</h4></li>
+                <li><h4>Careers</h4></li>
+                <li><h4>Privacy Policy</h4></li>
+            </ul>
         </div>
         <div class="shopping_online">
-            <h3>SHOPPING ONLINE</h3>
-            <h4>FAQ's</h4>
-            <h4>Delivery Information</h4>
-            <h4>Returns and Exchange</h4>
+            <h3>SHOPPING ONLINE <i class="arrow"></i></h3>
+            <ul class="shopping-links">
+                <li><h4>FAQ's</h4></li>
+                <li><h4>Delivery Information</h4></li>
+                <li><h4>Returns and Exchange</h4></li>
+            </ul>
         </div>
         <div class="contact_us">
             <h3>CONTACT US</h3>
@@ -27,26 +33,105 @@
     </div>
 </template>
 
+
+
 <style scoped>
     .footer {
         background-color: var(--book_brown);
-        position: fixed;
         width: 100%;
-        bottom: 0;
         display: flex;
+        flex-direction: row;
         justify-content: space-between;
-        padding: 20px;
-        
+        padding: 50px;
     }
     .account, .about_us, .shopping_online, .contact_us {
         padding: 20px;
     } 
     h3, h4 {
         color: var(--book_cream);
-        padding: 10px;
+        padding: 5px;
     }
     h3 {
         font-size: 16px;
     }
+    .logo {
+        display: flex;
+        flex-direction: column;
+        text-align: right;
+        justify-content: space-between;
+        padding: 20px;
+    }
+    .logo img {
+        max-width: 300px;
+
+    }
+    .logo::after {
+        content: "Copyright 2023";
+        color: var(--book_cream);
+        font-family: 'Quicksand_Regular';
+        padding-top: 50px;
+    }
+    .arrow {
+        border: solid var(--book_cream);
+        border-width: 0 3px 3px 0;
+        display: none;
+        padding: 3px;
+        transform: rotate(-45deg);
+
+    }
     
+    
+    
+    
+    
+    @media screen and (max-width: 700px) {
+        .footer {
+            flex-direction: column;
+        }
+        .logo {
+            align-items: center;
+            padding-top: 50px;
+        }
+        .logo img {
+            min-width: 380px;
+        }
+        .account-links, .about-links, .shopping-links {
+            display: none;
+        }
+        .account, .about_us, .shopping_online, .contact_us {
+            padding: 5px;
+        }
+        h4 {
+            display: block;
+            padding: 10px;
+        }
+        /* .account:hover .account-links,
+        .about_us:hover .about-links,
+        .shopping_online:hover .shopping-links {
+            display: block;
+            position: absolute;
+            background-color: var(--book_brown);
+            width: 100%;
+            margin: 0;
+            padding: 0;
+            list-style: none;
+            z-index: 1;
+        }
+        .account:hover .account-links li,
+        .about_us:hover .about-links li,
+        .shopping_online:hover .shopping-links li {
+            display: block;
+            padding: 8px;
+            border-top: 1px solid var(--book_cream);
+        }*/
+        .arrow {
+            display: inline-block;
+        } 
+        .about_us:hover > .about-links{display: block;}   
+        .about_us:hover > h3 .arrow{transform: rotate(45deg) !important;}
+        .account:hover > .account-links{display: block;}   
+        .account:hover > h3 .arrow{transform: rotate(45deg) !important;}
+        .shopping_online:hover > .shopping-links{display: block;}   
+        .shopping_online:hover > h3 .arrow{transform: rotate(45deg) !important;}     
+    }
 </style>

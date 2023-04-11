8eab3d3ba0809fec98d27243d97bea7185d8ef2d
Bolognesia <bele.ramirez@gmail.com>
2023-04-11 16:59:12 +1200

    router_view_dynamic_v.2

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,17 @@
+<script setup>
+  import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
+  
+</script>
+
 <template>
   <div>
-   
+    <Header />
   </div>
-</template>
-<script setup>
+  <div>
+   <router-view></router-view>
+  </div>
 
-</script>
+  <Footer />
+</template>
+
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,20 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-createApp(App)
-  .use(router)
-  .mount("#app");
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+// App.vue will be redirected to linked components via <router-links>
+// as a convention we will create new folder in 'src' named 'views' and place components to be linked in there
+// components related to those links needs to be defined below both for import and routes[] array
+import Home from '../views/Main.vue'
+const routes = [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+]
+// code below is NOT to be edited
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+  })
+  
+export default router

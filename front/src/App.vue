6c11838686f398ecf564d7a2439e753cdfd10f8f
Bolognesia <bele.ramirez@gmail.com>
2023-04-13 10:23:49 +1200

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
   import HeaderVue from './components/newHeader.vue';
-  // import LoginVue from './components/LoginModal.vue'
+
 </script>
 
 <template>
   <HeaderVue />
-
-  <div class="login-modal">
-    <!-- <LoginVue /> -->
-  </div>
+  
   <div>
     <router-view></router-view>
   </div>

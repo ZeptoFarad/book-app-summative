9b74435e798ff0764aaf55d362088f3534045af7
LiamPatman <270025210@yoobeestudent.ac.nz>
2023-04-12 17:34:34 +1200

    Main Clear
    Just cleared main for less conflicts

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
-  import MyBookshop from './components/MyBookshop.vue';
-  import Footer from './components/Footer.vue';
+  
 </script>
 
 <template>
   <div>
-    <MyBookshop />
-    <Footer />
+    
   </div>
 
 </template>

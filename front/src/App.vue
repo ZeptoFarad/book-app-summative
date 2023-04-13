ab8391de26d44f9d150b84ccda0bffabbdc96ba6
LiamPatman <270025210@yoobeestudent.ac.nz>
2023-04-14 08:52:25 +1200

    Javascript Added for MyBookshop

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
   import HeaderVue from './components/newHeader.vue';
+  import MyBookshop from './components/MyBookshop.vue';
+  import Footer from './components/Footer.vue'
 </script>
 
 <template>
 
   <div>
-    
+    <MyBookshop />
+    <Footer />
   </div>
 
 
